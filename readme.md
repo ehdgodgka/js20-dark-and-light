029a1c6f6ee768006de8e6d4292dd37c4291f9d4
soodang <ehdgodgka@gmail.com>
2020-08-13 17:02:36 +0900

    code review , cleanup codes

--- readme.md
+++ readme.md
@@ -1,14 +1,17 @@
 # light and dark ( Hello Seoul)
 <img src='assets/demo.gif'>
 
-## about
+## feature
 - An site that practice changes styles with only JS and CSS dynamically.
 - There are light mode and dark mode. each mode corresponds to day and night of Seoul.
+- save theme mode
+- responsive screen
+
 ---
-## code info
+## code
   - Built with HTML, CSS and Javascript.
-  - responsive screen
-  - dark,light mode change dynamically using custom data attribute
+  - by togglec custom data attribute, change style.
+  - theme mode save in local storage.
 
 referenced ideas and codes from the ZMT JS course
 [course-link](https://academy.zerotomastery.io/p/javascript-projects)
--- script.js
+++ script.js
@@ -8,48 +8,63 @@ const contactImg = document.getElementById('contact').querySelector('img');
 const toggleSwitchWrapper = document.getElementById('theme-switch-wrapper');
 const toggleSwitchIcon = toggleSwitchWrapper.querySelector('i');
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const themeStorage = window.localStorage;
 
 //switch theme dynamically
 const applyTheme = () => {
   if (toggleSwitch.checked) {
     //dark-mode
+    themeStorage.setItem('hello-seoul-theme', 'dark');
     document.documentElement.setAttribute('data-theme', 'dark');
     toggleSwitchIcon.className = 'fas fa-moon';
+
+    // hello seoul section
     imgAuthorLink.textContent = `author by ${imgDarkLinks.background.author}`;
     imgAuthorLink.setAttribute('href', imgDarkLinks.background.link);
     helloSeoulSection.style.backgroundImage = `url('assets/seoul-img/background_dark.jpg')`;
 
+    //peek section
     for (let i = 0; i < 3; i++) {
       peekImgs[i].setAttribute('src', `assets/seoul-img/img${i + 1}_dark.jpg`);
       imgAnchors[i].setAttribute('href', imgDarkLinks[i]);
     }
+    //event section
     eventTitle.textContent = 'night event';
     events.forEach((event, index) => {
       event.querySelector('h2').textContent = imgDarkLinks.events[index].title;
       event.querySelector('img').setAttribute('src', imgDarkLinks.events[index].link);
     });
+    //contact section
     contactImg.setAttribute('src', 'assets/contact_dark.svg');
   } else {
     //light-mode
-    document.documentElement.removeAttribute('data-theme');
+    themeStorage.setItem('hello-seoul-theme', 'light');
+    document.documentElement.setAttribute('data-theme', 'light');
     toggleSwitchIcon.className = 'fas fa-sun';
+
+    //hello-seoul section
     imgAuthorLink.textContent = `author by ${imgLightLinks.background.author}`;
     imgAuthorLink.setAttribute('href', imgLightLinks.background.link);
     helloSeoulSection.style.backgroundImage = `url('assets/seoul-img/background_light.jpg')`;
 
+    //peek section
     for (let i = 0; i < 3; i++) {
       peekImgs[i].setAttribute('src', `assets/seoul-img/img${i + 1}_light.jpg`);
       imgAnchors[i].setAttribute('href', imgLightLinks[i]);
     }
-    eventTitle.textContent = 'day event';
 
+    //event section
+    eventTitle.textContent = 'day event';
     events.forEach((event, index) => {
       event.querySelector('h2').textContent = imgLightLinks.events[index].title;
       event.querySelector('img').setAttribute('src', imgLightLinks.events[index].link);
     });
+
+    //contact section
     contactImg.setAttribute('src', 'assets/contact_light.svg');
   }
 };
 
 toggleSwitch.addEventListener('change', applyTheme);
+toggleSwitch.checked = themeStorage.getItem('hello-seoul-theme') === 'dark' ? true : false;
 applyTheme();
