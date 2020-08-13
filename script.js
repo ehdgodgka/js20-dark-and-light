4f892643f9a7f5d7ae2978b783ac878db9ee6407
soodang <ehdgodgka@gmail.com>
2020-08-13 17:34:14 +0900

    improve code readability : theme

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ const toggleSwitchWrapper = document.getElementById('theme-switch-wrapper');
 const toggleSwitchIcon = toggleSwitchWrapper.querySelector('i');
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const themeStorage = window.localStorage;
+const DARK_THEME = 'dark';
+const LIGHT_THEME = 'light';
+
 
 //switch theme dynamically
 const applyTheme = () => {
-  const theme = toggleSwitch.checked ? 'dark' : 'light';
-  const src = toggleSwitch.checked ? imgDarkLinks : imgLightLinks;
+  const theme = toggleSwitch.checked ? DARK_THEME : LIGHT_THEME;
+  const src = theme === DARK_THEME ? imgDarkLinks : imgLightLinks;
 
   //theme mode
   themeStorage.setItem('hello-seoul-theme', theme);
   document.documentElement.setAttribute('data-theme', theme);
-  toggleSwitchIcon.className = toggleSwitch.checked ? 'fas fa-moon' : 'fas fa-sun';
+  toggleSwitchIcon.className = theme === DARK_THEME? 'fas fa-moon' : 'fas fa-sun';
 
   // hello seoul section
   imgAuthorLink.textContent = `author by ${src.background.author}`;
@@ -32,7 +35,7 @@ const applyTheme = () => {
   }
 
   //event section
-  eventTitle.textContent = toggleSwitch.checked ? 'night event' : 'day event';
+  eventTitle.textContent = theme === DARK_THEME ? 'night event' : 'day event';
   events.forEach((event, index) => {
     event.querySelector('h2').textContent = src.events[index].title;
     event.querySelector('img').setAttribute('src', src.events[index].link);
